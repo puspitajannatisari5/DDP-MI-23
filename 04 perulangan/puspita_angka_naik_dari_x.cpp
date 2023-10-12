ede99db2bd69bf630919c188d01c55a20b38fe90
puspitajannatisari5 <puspitasari7980@gmail.com>
2023-10-12 20:22:59 +0700

    tugas perulangan

---
+++ 04 perulangan/puspita_angka_naik_dari_x.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+int main () {
+    int a,b;
+    cin >> a>>b;
+    // 2<5
+    for (int i = a; i <= b; i++) { 
+        cout <<i<< " ";
+     }
+    
+    return 0;
+}
