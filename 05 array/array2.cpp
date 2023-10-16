09465d5806750ff5c0ac5c1b3539e82b053676f4
puspitajannatisari5 <puspitasari7980@gmail.com>
2023-10-16 19:51:13 +0700

    materi array

---
+++ 05 array/array2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nilai[5];
+
+    nilai[0] = 32;
+    nilai[1] = 42;
+    nilai[2] = 76;
+    nilai[3] = 31;
+    nilai[4] = 57;
+
+   for (int i=4; i < 1; i++){
+    cout << "Nilai ke-" <<i<< "="<< nilai[i] << endl;
+   }
+
+return 0;
+
+}
